f66c10bf12d2b8c457b0c8a378c5937e2dd53f91
diogocnew <52328198+diogocnew@users.noreply.github.com>
2025-07-20 21:58:43 +0000

    Alterando o resultado da index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return <h1>Alteração de mensagem para Teste a ser realizado com a finalidade de sentir
+        a alteração feita diretamente no git.
+    </h1>
+}
+
+
+
+export default Home;
+ 
